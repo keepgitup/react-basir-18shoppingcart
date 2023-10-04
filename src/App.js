08235643eecb39cfe55e0472758d7d6078f8bf40
keep <flymayday112@gmail.com>
2023-10-04 08:01:40 +0800

    add src data-js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Header from './components/header';
 import Basket from './components/Basket';
 import Main from './components/Main';
-import './App.css';
+// import './App.css';
+import data from './data';
 
 function App() {
+  const {products}=data;
   return (
     <div>
      <Header />
      <div>
-      <Main />
+      <Main products={products} />
 <Basket />
 
      </div>
---
+++ src/data.js
@@ -0,0 +1,27 @@
+const data = {
+products:[
+{
+    id:'1',
+    name:'Macbook',
+    price:1400,
+    image:'https://picsum.photos/id/180/2400/1600',
+},
+{
+    id:'2',
+    name:'Old woman bag',
+    price:12400,
+    image:'https://picsum.photos/id/180/2400/1600',
+},
+{
+    id:'3',
+    name:'notebook',
+    price:400,
+    image:'https://picsum.photos/id/180/2400/1600',
+},
+
+],
+
+
+};
+
+export default data;
